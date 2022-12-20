e17e3b98750bc56f947856be39d58dcc3aa9bc05
Àngel Camps <ausias@hINFO6-4.xiam.cat>
2022-12-20 12:21:15 +0100

    Mañana no hay classe2

--- TestM05_19DIC/src/testm05_19dic/TestM05_19DIC.java
+++ TestM05_19DIC/src/testm05_19dic/TestM05_19DIC.java
@@ -23,7 +23,7 @@ public class TestM05_19DIC {
             System.out.println("Valor de i :" + i);
         }
         
-        System.out.println("La bebesita bebe lean");
+        System.out.println("La macarena");
     }
     
 }
